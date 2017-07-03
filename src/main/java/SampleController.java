23c24bd812225ee9e5e716f196256886ff7ecf31
renpengfei <rpf@rrmj.tv>
2017-07-03 20:41:09 +0800

    Initial commit

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,27 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author
+ * @create 2017-07-03 13:58
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
