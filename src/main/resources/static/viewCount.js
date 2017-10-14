66d1ea7de302c14a1001867f68647652c6b967eb
ryan <249900703@qq.com>
2017-10-14 17:52:46 +0800

    添加百度统计

---
+++ src/main/resources/static/viewCount.js
@@ -0,0 +1,12 @@
+/**
+ * Created by admin on 2017/10/14.
+ */
+<script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?d31da9a61b256ea800dcd1cff22280e2";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
